42d3d5aaf820e23586af9149636bf640ddbcdd13
Developer-Umarbek <umarbektugalov@gmail.com>
2022-08-20 18:40:58 +0500

    Js tutorial

---
+++ script.js
@@ -0,0 +1,124 @@
+"use strick";
+
+// ============= 1-m =====================
+
+
+// let a = 5;
+
+// let b = a * a * a 
+
+// console.log(b);
+
+
+// ============== 2-m ================
+
+// let a = 10;
+
+// let b = 23;
+
+// let c = a + b;
+
+// console.log(c)
+
+// ================= 3-m ================
+
+// let a = 10;
+
+// let b = 3;
+
+// let c = a - b;
+
+// console.log(c)
+
+// =============== 4-m ================
+
+
+
+// function isName() {
+//     let a = 5;
+//     let b =  a * a ; 
+//     let c = a * a * a;
+//     let d = a * a * a * a;
+//     console.log(b  , c , d)
+// }
+// isName()
+
+// ============== 5-m ==================
+
+// function toq(){
+//     for (a = 1; a <= 20; a++){
+//         if(a % 2 == 1){
+//             console.log(a);
+//         }
+//     }
+// }
+// toq()
+
+// ============== 6-m =================
+
+
+// function juft(){
+//     for (a = 1; a <= 50; a++){
+//         if(a % 2 == 0){
+//             console.log(a);
+//         }
+//     }
+// }
+// juft()
+
+
+// ============= 7-m ================
+
+// =============== 8-m ==============
+
+//================ 9-m =================
+
+
+// function plus() {
+
+//     let a = document.getElementById('npt1').value * 1;
+//     let b = document.getElementById('npt2').value * 1;
+//     let result = a + b
+//     document.write(result);
+
+// } 
+
+// function minus() {
+
+//     let a = document.getElementById('npt1').value * 1;
+//     let b = document.getElementById('npt2').value * 1;
+//     let result = a - b
+//     document.write(result);
+
+// }
+
+// function bolish() {
+
+//     let a = document.getElementById('npt1').value * 1;
+//     let b = document.getElementById('npt2').value * 1;
+//     let result = a / b
+//     document.write(result);
+
+// }
+
+// function kopaytma() {
+
+//     let a = document.getElementById('npt1').value * 1;
+//     let b = document.getElementById('npt2').value * 1;
+//     let result = a * b
+//     document.write(result);
+
+// }
+
+
+
+
+// =============== 10-m =============
+
+// function karraJadval(){
+//     for (let a = 1;  a <= 10 ; a++) {
+//         for (let b = 1; b <= 10; b++)
+//         document.write(`${a} x ${b} = ${a * b} <br>`)
+//     }
+// }
+// karraJadval()
